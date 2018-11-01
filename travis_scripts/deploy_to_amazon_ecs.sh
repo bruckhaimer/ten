5f35e810b9e5d7182c38a1d901fb1267ed492a5a
bruckhaimer <44107357+bruckhaimer@users.noreply.github.com>
2018-11-01 10:55:48 +0800

    Update deploy_to_amazon_ecs.sh

--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,4 +1,4 @@
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure --region us-west-1 --access-key AKIAJTA5RMP7AREEUGBQ --secret-key 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 --cluster spmia-tmx-dev
+ecs-cli configure --region us-west-1 --access-key --secret-key  --cluster spmia-tmx-dev
 ecs-cli compose --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
